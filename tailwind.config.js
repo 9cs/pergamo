d30f2c12aedba0774385c06f2ba310d8663ba91d
Dantton <dantton.pinheiro@ucsal.edu.br>
2025-08-20 10:27:22 -0300

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',      // Next.js App Router
-    './pages/**/*.{js,ts,jsx,tsx}',    // caso tenha pages
-    './components/**/*.{js,ts,jsx,tsx}', // componentes
-  ],
+  content: [],
   theme: {
     extend: {},
   },
